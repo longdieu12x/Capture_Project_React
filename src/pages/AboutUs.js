c8046fa2c9bf5f5d8fe8dc447c5100fdc12f655f
longdieu12x <longdieu12x@gmail.com>
2021-08-08 15:23:10 +0700

    Service Section in About

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 const AboutUs = () => {
     return (
-        <AboutSection/>
+        <React.Fragment>
+            <AboutSection/>
+            <ServicesSection/>
+        </React.Fragment>
     );
 }
 
